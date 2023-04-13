7a6048e75ff29443909b70fe5ec730def61044eb
庄建国 <zhuangjianguo@steedos.com>
2023-04-13 09:41:25 +0000

    node red

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const lodash = require('lodash');
 // https://nodered.org/docs/user-guide/runtime/configuration
 
 module.exports = {
-    flowFile: path.join(__dirname, 'src', 'flows.json'),
+    flowFile: path.join(__dirname, 'flows', 'default.json'),
     flowFilePretty: true,
     credentialSecret: process.env.NODERED_CREDENTIAL_SECRET || 'steedos',
     userDir: path.join(__dirname, '.node-red'),
